d5d2b71c23a834a096e0b8ea626c185df51ffef2
GogoK <georgiakanchev@gmail.com>
2015-10-15 15:44:55 +0300

    sort array of numbers

--- 01.ArraysListsStacksQueues/01.SortArrayOfNumbers/SortArrayOfNumbers.cs
+++ 01.ArraysListsStacksQueues/01.SortArrayOfNumbers/SortArrayOfNumbers.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿/*  Write a program to read an array of numbers from the console, 
+    sort them and print them back on the console. 
+    The numbers should be entered from the console on a single line, separated by a space.
+
+*/
+
+using System;
+using System.Linq;
 
 class SortArrayOfNumbers
 {
     static void Main()
     {
-        Console.WriteLine("Sorting a array");
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Array.Sort(numbers);
+        Console.WriteLine(string.Join(" ", numbers));
     }
 }
