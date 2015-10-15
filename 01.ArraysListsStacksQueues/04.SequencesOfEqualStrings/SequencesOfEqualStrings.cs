8fa246ed5869951734def7f54fdad617995a3a59
GogoK <georgiakanchev@gmail.com>
2015-10-15 19:16:43 +0300

    sequences of equal strings

---
+++ 01.ArraysListsStacksQueues/04.SequencesOfEqualStrings/SequencesOfEqualStrings.cs
@@ -0,0 +1,28 @@
+﻿/*  Write a program that reads an array of strings and finds in it all sequences of equal elements (comparison should be case-sensitive). The input strings are given as a single line, separated by a space.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SequencesOfEqualStrings
+{
+    static void Main()
+    {
+        List<string> strList = Console.ReadLine().Split(' ').ToList();
+
+        while (strList.Count > 0)
+        {
+            string current = strList[0];
+            for (int j = 0; j < strList.Count; j++)
+            {
+                if (current == strList[j])
+                {
+                    Console.Write("{0} ", strList[j]);
+                }
+            }
+            Console.WriteLine();
+            strList.RemoveAll(str => str == current);
+        }
+    }
+}
