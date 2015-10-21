06c4245f20ff1e7003620e9c868ee1e805138b3d
GogoK <georgiakanchev@gmail.com>
2015-10-21 04:26:33 +0300

    count symbols in string

---
+++ 02.MultiArraysSetsDictionaries/06.CountSymbols/CountSymbols.cs
@@ -0,0 +1,33 @@
+﻿/*  Write a program that reads some text from the console and counts the occurrences of each character in it. 
+    Print the results in alphabetical (lexicographical) order.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class CountSymbols
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        SortedDictionary<char, int> letters = new SortedDictionary<char, int>();
+        foreach (var letter in input)
+        {
+            if (!letters.ContainsKey(letter))
+            {
+                letters.Add(letter, 1);
+            }
+            else
+            {
+                int value = letters[letter];
+                letters[letter] = ++value;
+            }
+        }
+
+        foreach (var ch in letters)
+        {
+            Console.WriteLine("{0}: {1} time/s", ch.Key, ch.Value);
+        }
+    }
+}
