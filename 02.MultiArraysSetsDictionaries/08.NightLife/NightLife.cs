e14435c2b92dc71b212592fbed946f74ac31230b
GogoK <georgiakanchev@gmail.com>
2015-10-21 06:55:38 +0300

    night life

---
+++ 02.MultiArraysSetsDictionaries/08.NightLife/NightLife.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class NightLife
+{
+    static void Main()
+    {
+        Dictionary<string, SortedDictionary<string, SortedSet<string>>> performances = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+        string input = Console.ReadLine();
+
+        while (input != "END")
+        {
+            string[] cityVenuePerformer = input.Split(';');
+            string city = cityVenuePerformer[0];
+            string venue = cityVenuePerformer[1];
+            string performer = cityVenuePerformer[2];
+
+            if (!performances.ContainsKey(city))
+            {
+                performances[city] = new SortedDictionary<string, SortedSet<string>>();
+            }
+            if (!performances[city].ContainsKey(venue))
+            {
+                performances[city][venue] = new SortedSet<string>();
+            }
+            if (!performances[city][venue].Contains(performer))
+            {
+                performances[city][venue].Add(performer);
+            }
+
+            input = Console.ReadLine();
+        }
+        Console.WriteLine();
+
+        foreach (var cityVenue in performances)
+        {
+            Console.WriteLine(cityVenue.Key);
+
+            foreach (var venueWithPerformers in cityVenue.Value)
+            {
+                Console.WriteLine("->{0}: {1}", venueWithPerformers.Key, string.Join(", ", venueWithPerformers.Value));
+            }
+        }
+    }
+}
