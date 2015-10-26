aa54e7aef4b35e459de3598b24f1d479e7a08775
GogoK <georgiakanchev@gmail.com>
2015-10-26 20:21:08 +0200

    return last digit of number, as word

---
+++ 03.Methods/02.LastDigitOfNumber/LastDigitOfNumber.cs
@@ -0,0 +1,21 @@
+﻿/*  Write a method that returns the last digit of a given integer as an English word.
+    Test the method with different input values. Ensure you name the method properly.
+*/
+
+using System;
+
+class LastDigitOfNumber
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetLastDigitAsWord(number));
+    }
+
+    static string GetLastDigitAsWord(int number)
+    {
+        string[] digitWords = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        int index = number % 10;
+        return digitWords[index];
+    }
+}
