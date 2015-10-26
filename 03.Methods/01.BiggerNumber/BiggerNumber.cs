265e5fe8e40b99d170520f080787960c574d3ae2
GogoK <georgiakanchev@gmail.com>
2015-10-26 19:44:09 +0200

    bigger number

---
+++ 03.Methods/01.BiggerNumber/BiggerNumber.cs
@@ -0,0 +1,27 @@
+﻿/*  Write a method GetMax() with two parameters that returns the larger of two integers.
+    Write a program that reads 2 integers from the console and prints the largest of them using the method GetMax().
+*/
+
+using System;
+
+class BiggerNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetMax(firstNumber, secondNumber));
+    }
+
+    static int GetMax(int first, int second)
+    {
+        if (first > second)
+        {
+            return first;
+        }
+        else
+        {
+            return second;
+        }
+    }
+}
