5e9f42a8a1db503a4117b1788445a5680727ed30
GogoK <georgiakanchev@gmail.com>
2015-10-26 21:46:00 +0200

    reverse number

---
+++ 03.Methods/05.ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿/*  Write a method that reverses the digits of a given floating-point number.
+*/
+
+using System;
+
+class ReverseNumber
+{
+    static void Main()
+    {
+        char[] input = Console.ReadLine().ToCharArray();
+        double reversed = double.Parse(ReverseCharArray(input));
+        Console.WriteLine(reversed);
+    }
+
+    static string ReverseCharArray(char[] input)
+    {
+        string reveredArray = string.Empty;
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reveredArray += input[i];
+        }
+        return reveredArray;
+    }
+}
