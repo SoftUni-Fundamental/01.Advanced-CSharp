e7984d890099890ada170c7abb0153d079318c9c
GogoK <georgiakanchev@gmail.com>
2015-10-28 00:39:23 +0200

    count substring occurrences

---
+++ 04.StringsAndTextProcessing/03.CountSubstringOccurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,27 @@
+﻿/*  Write a program to find how many times a given string appears in a given text as substring. 
+    The text is given at the first input line. 
+    The search string is given at the second input line.
+    The output is an integer number. 
+    Please ignore the character casing.
+    Overlapping between occurrences is allowed.
+*/
+
+using System;
+
+class CountSubstringOccurrences
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string subStr = Console.ReadLine();
+
+        int counter = 0;
+        int i = 0;
+        while ((i = input.IndexOf(subStr, i, StringComparison.CurrentCultureIgnoreCase)) != -1)
+        {
+            i++;
+            counter++;
+        }
+        Console.WriteLine(counter);
+    }
+}
