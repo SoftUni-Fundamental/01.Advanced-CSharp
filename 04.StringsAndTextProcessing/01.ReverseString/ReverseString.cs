b8e9d5dc789e2fb3853a07383f803f1482009892
GogoK <georgiakanchev@gmail.com>
2015-10-27 20:21:48 +0200

    reverse string

---
+++ 04.StringsAndTextProcessing/01.ReverseString/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿/*  Write a program that reads a string from the console, reverses it and prints the result back at the console.
+*/
+
+using System;
+
+class ReverseString
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string output = string.Empty;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            int index = (input.Length - 1) - i;
+            output += input[index];
+        }
+        Console.WriteLine(output);
+    }
+}
