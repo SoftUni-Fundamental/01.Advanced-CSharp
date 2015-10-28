26942c6e07df698fe5ebb515f704e9bac2ce4da1
GogoK <georgiakanchev@gmail.com>
2015-10-28 02:21:28 +0200

    string chars to unicode

---
+++ 04.StringsAndTextProcessing/05.UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,17 @@
+﻿/*  Write a program that converts a string to a sequence of C# Unicode character literals.
+*/
+
+using System;
+
+class UnicodeCharacters
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        foreach (var ch in str)
+        {
+            Console.Write("\\u{0:X4}", Convert.ToUInt32(ch));
+        }
+        Console.WriteLine();
+    }
+}
