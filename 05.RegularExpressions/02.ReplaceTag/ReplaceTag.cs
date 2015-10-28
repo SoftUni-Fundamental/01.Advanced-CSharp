448cee7588c2cee4bf6515628b1ebe888cdc887f
GogoK <georgiakanchev@gmail.com>
2015-10-28 23:51:46 +0200

    relpace tags

---
+++ 05.RegularExpressions/02.ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ReplaceTag
+{
+    static void Main()
+    {
+        string input = @"<ul>
+ <li>
+  <a href=""http://softuni.bg"">SoftUni</a>
+ </li>
+</ul>";
+        string pattern = @"<a\s+href=([^>]+)>([^<]+)</a>";
+        string replacement = "[URL href=$1]$2[/URL]";
+        Regex regex = new Regex(pattern);
+
+        string output = Regex.Replace(input, pattern, replacement);
+        Console.WriteLine(output);
+    }
+}
