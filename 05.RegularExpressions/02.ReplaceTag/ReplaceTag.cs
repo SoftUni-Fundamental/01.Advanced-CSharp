3a7d0a45e0850bdf7df05f72f309a43ac47af711
GogoK <georgiakanchev@gmail.com>
2015-10-30 08:42:44 +0200

    replace tags(<a> to [URL])

--- 05.RegularExpressions/02.ReplaceTag/ReplaceTag.cs
+++ 05.RegularExpressions/02.ReplaceTag/ReplaceTag.cs
@@ -5,16 +5,14 @@ class ReplaceTag
 {
     static void Main()
     {
-        string input = @"<ul>
- <li>
-  <a href=""http://softuni.bg"">SoftUni</a>
- </li>
-</ul>";
-        string pattern = @"<a\s+href=([^>]+)>([^<]+)</a>";
+        string input = @" <ul><li><a href=""http://softuni.bg"">SoftUni</a></li></ul>""
+""<ul><li><a href='http://softuni.bg'>SoftUni</a></li></ul>";
+        
+        string pattern = @"<a\s+href=.([^>]+).>([^<]+)</a>";
         string replacement = "[URL href=$1]$2[/URL]";
-        Regex regex = new Regex(pattern);
 
-        string output = Regex.Replace(input, pattern, replacement);
-        Console.WriteLine(output);
+        Regex regex = new Regex(pattern);
+        string result = Regex.Replace(input, pattern, replacement);
+        Console.WriteLine(result);
     }
 }
