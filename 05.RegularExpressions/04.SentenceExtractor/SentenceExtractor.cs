cf99a756d37fb1821ef4e120f4d6bc2c98222d7e
GogoK <georgiakanchev@gmail.com>
2015-10-31 12:51:22 +0200

    sentence extractor

---
+++ 05.RegularExpressions/04.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SentenceExtractor
+{
+    class SentenceExtractor
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
