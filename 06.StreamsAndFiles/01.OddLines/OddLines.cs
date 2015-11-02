c462dd7f65b9583f1d688eb755a6271feb975833
GogoK <georgiakanchev@gmail.com>
2015-11-02 23:45:52 +0200

    read/print odd lines

---
+++ 06.StreamsAndFiles/01.OddLines/OddLines.cs
@@ -0,0 +1,34 @@
+﻿/*  Write a program that reads a text file and prints on the console its odd lines. 
+    Line numbers starts from 0.
+    Use StreamReader.
+*/
+
+using System;
+using System.IO;
+using System.Text;
+
+class OddLines
+{
+    static void Main()
+    {
+        int counter = 0;
+        string line;
+
+        StreamReader file = new StreamReader("../../ten-lines.txt");
+        try
+        {
+            while ((line = file.ReadLine()) != null)
+            {
+                if (counter % 2 == 1)
+                {
+                    Console.WriteLine(line);
+                }
+                counter++;
+            }
+        }
+        finally
+        {
+            file.Close();
+        }       
+    }
+}
